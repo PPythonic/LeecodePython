1edc7e2b31faf3531688ed01b8d5cbbfeba38c49
wangjianan <1358314685@qq.com>
2018-06-12 18:13:10 +0800

    合并两个有序数组

---
+++ sort&search/merge.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+
+"""
+给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+说明:
+初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+"""
+
+"""
+示例
+输入:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+输出: [1,2,2,3,5,6]
+"""
+
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        i, j, k = m-1, n-1, m+n-1
+
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        
+        if i < 0:
+            nums1[:k+1] = nums2[:j+1]
+
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+s = Solution()
+s.merge(nums1, m, nums2, n)
+print nums1
